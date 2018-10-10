4dbd5802ca11695c55eff6b7f52fc13e98669eae
SaulGarGar <saul.alejandro.garcia.garcia@gmail.com>
2018-10-10 03:42:10 -0500

    :octocat: Add the fragments to the bottom navigation.

--- app/src/main/java/com/saulgargar/library_system/activities/DashboardActivity.java
+++ app/src/main/java/com/saulgargar/library_system/activities/DashboardActivity.java
@@ -1,15 +1,82 @@
 package com.saulgargar.library_system.activities;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.saulgargar.library_system.R;
+import com.saulgargar.library_system.fragments.CatalogAuthorFragment;
+import com.saulgargar.library_system.fragments.CatalogBookFragment;
+import com.saulgargar.library_system.fragments.CatalogUsersFragment;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        setBottomNavigationView();
+        setFragmentByDefault();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        finish();
+    }
+
+    public void setBottomNavigationView(){
+
+        bottomNavigationView = findViewById(R.id.navigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                boolean fragmentTransaction = false;
+                Fragment fragment = null;
+
+                switch (item.getItemId()){
+                    case R.id.books:
+                        fragment = new CatalogBookFragment();
+                        fragmentTransaction = true;
+                        break;
+                    case R.id.authors:
+                        fragment = new CatalogAuthorFragment();
+                        fragmentTransaction = true;
+                        break;
+                    case R.id.users:
+                        fragment = new CatalogUsersFragment();
+                        fragmentTransaction = true;
+                        break;
+                }
+
+                if (fragmentTransaction){
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.container,fragment)
+                            .commit();
+                    item.setChecked(true);
+
+                }
+
+                return true;
+            }
+        });
+    }
+
+    private void setFragmentByDefault(){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, new CatalogBookFragment())
+                .commit();
+
     }
 }
